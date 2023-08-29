1b0b10fa85add9fe1045f33f7d347f27c16ba991
tomined <109042695+tomined@users.noreply.github.com>
2023-08-29 19:39:25 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "CHANGE_ME_RESOURCE_GROUP"
+  default = "python_gruppa_zasoboow"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "CHANGE_ME_APP_NAME"
+  default     = "aplikacja_python_cosTestowego"
 }
 
 variable "location" {
